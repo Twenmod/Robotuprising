03ed60c4b1c739c4a5e6a7b557b99d4ad191b7a8
twenmod <twengames@gmail.com>
2023-11-30 12:15:05 +0100

    info test

--- routes/info.js
+++ routes/info.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/info', function(req, res, next) {
+  res.render('info.pug', { title: 'Express', data});
 });
 
 module.exports = router;
